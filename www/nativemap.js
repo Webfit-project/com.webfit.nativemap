3dd6761bf9adb11297492bf88e23986a3bf6e899
Nicolas <nicolas13008@gmail.com>
2017-03-15 14:27:49 +0100

    update nativemap.js

--- www/nativemap.js
+++ www/nativemap.js
@@ -1,17 +1,32 @@
 //cordova.define('com.webfit.nativemap.nativemap', function(require, exports, module) {
+if (typeof cordova !== 'undefined') {
+    var exec = require('cordova/exec');
 
-var exec = require('cordova/exec');
+    var nativemap = {
 
+        startMap : function(successCallback, failureCallback) {
+
+            exec(successCallback, failureCallback, 'nativemap', 'startListen', []);
+        }
+    }
+
+
+    module.exports = nativemap;
+}
+else
+{
 var nativemap = {
 
-    startMap : function(successCallback, failureCallback) {
+        startMap : function(successCallback, failureCallback) {
 
-        exec(successCallback, failureCallback, 'nativemap', 'startListen', []);
+            console.log("not work on navigator");
+        }
     }
+
+
+    module.exports = nativemap;
 }
-
-
-module.exports = nativemap;
+}
 //cordova.define("cordova/plugin/gpsDetectionPlugin", gpsDetect);
 
 //});
