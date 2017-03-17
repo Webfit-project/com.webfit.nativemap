05a89d2322908bde5e4e97e8661a81db53486457
Nicolas <nicolas13008@gmail.com>
2017-03-17 19:20:11 +0100

    work in progress

--- src/android/MapActivity.java
+++ src/android/MapActivity.java
@@ -57,18 +57,51 @@ public class MapActivity extends Activity implements View.OnClickListener {
     String iconList = intent.getStringExtra("iconList");
     String route =  intent.getStringExtra("route");
 
+
+
     mapView = (MapView) findViewById(R.id.map);
     mapView.setTileSource(TileSourceFactory.MAPNIK);
-
+    mapView.setLayerType(View.LAYER_TYPE_SOFTWARE,null);
     IMapController mapController = mapView.getController();
+    try {
+      JSONObject centerObject = new JSONObject(center);
+      GeoPoint point2 = new GeoPoint(centerObject.getDouble("lat"), centerObject.getDouble("lon"));
+      mapController.setCenter(point2);
+    } catch (JSONException e) {
+      e.printStackTrace();
+      GeoPoint point2 = new GeoPoint(44.923001,  6.359711);
+      mapController.setCenter(point2);
+    }
+
     mapController.setZoom(15);
-    mapView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     mapView.setMaxZoomLevel(25);
-    GeoPoint point2 = new GeoPoint(44.923001,  6.359711);
-    mapController.setCenter(point2);
+
     mapView.setMultiTouchControls(true);
     mapView.setTilesScaledToDpi(true);
     final DisplayMetrics dm = ctx.getResources().getDisplayMetrics();
+
+    mapView.setMapListener(new MapListener() {
+      @Override
+      public boolean onScroll(ScrollEvent event) {
+        return false;
+      }
+
+      @Override
+      public boolean onZoom(ZoomEvent event) {
+        if(mapView.getZoomLevel() < 13)
+        {
+          mapView.setLayerType(View.LAYER_TYPE_HARDWARE,null);
+
+        }
+        else
+        {
+          mapView.setLayerType(View.LAYER_TYPE_SOFTWARE,null);
+
+        }
+        return false;
+      }
+
+    } );
 
 
 
@@ -86,61 +119,38 @@ public class MapActivity extends Activity implements View.OnClickListener {
 
     mapView.getOverlays().add(this.mScaleBarOverlay);
     Log.d("STATE","iconlist in activity = "+ iconList);
-    mapView.setMapListener(new MapListener() {
-      @Override
-      public boolean onScroll(ScrollEvent event) {
-        return false;
-      }
-
-      public boolean onZoom(ZoomEvent arg0) {
-        Log.d("STATE","ZOOM="+mapView.getZoomLevel());
-        if(mapView.getZoomLevel() > 13)
-        {
-          Log.d("STATE","SOFTWARE");
-          mapView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        }
-        else
-        {
-          Log.d("STATE","HARDWARE");
-          mapView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-        }
-        return false;
-      }
-
-
-    } );
-
     this.addItem(iconList);
     this.addRoute(route);
 
-
   }
 
-  public void addRoute(String route)
-  {
-    PathOverlay myPath = new PathOverlay(Color.argb(95,39,185,0), this);
+  public void addRoute(String route) {
+    PathOverlay myPath = new PathOverlay(Color.argb(95, 39, 185, 0), this);
+
+
     try {
       JSONObject iconObject = new JSONObject(route);
       JSONArray jArray = iconObject.getJSONArray("list");
 
       for (int i = 0; i < jArray.length(); i++) {
         JSONObject json_data = jArray.getJSONObject(i);
-        GeoPoint gpt= new GeoPoint(json_data.getDouble("lat"),  json_data.getDouble("lon"));
+        GeoPoint gpt = new GeoPoint(json_data.getDouble("lat"), json_data.getDouble("lon"));
         myPath.addPoint(gpt);
       }
-    }
-    catch (JSONException e) {
+    } catch (JSONException e) {
       e.printStackTrace();
     }
 
+
     Paint pPaint = myPath.getPaint();
     pPaint.setStyle(Paint.Style.STROKE);
-    pPaint.setStrokeWidth(8);
+    pPaint.setStrokeWidth(10);
     myPath.setPaint(pPaint);
 
 
     mapView.getOverlays().add(myPath);
   }
+
 
   public void addItem(String iconList)
   {
@@ -164,10 +174,125 @@ public class MapActivity extends Activity implements View.OnClickListener {
 
 
         olItem = new OverlayItem(json_data.getString("title"), json_data.getString("description"),new GeoPoint(json_data.getDouble("lat"), json_data.getDouble("lon")));
+        Drawable newMarker= null;
         switch(json_data.getString("icon"))
         {
           case "ic_sommet":
-            Drawable newMarker = this.getResources().getDrawable(R.drawable.icon_sommet);
+            newMarker = this.getResources().getDrawable(R.drawable.icon_sommet);
+            olItem.setMarker(newMarker);
+            break;
+          case "icon_abri":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_abri);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_acces":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_acces);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_atterrissage":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_atterrissage);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_bisse":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_bisse);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_bivouac":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_bivouac);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_camp":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_camp);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_camping":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_camping);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_canyon":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_canyon);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_cascade":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_cascade);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_col":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_col);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_decollage":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_decollage);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_escalade":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_escalade);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_gite":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_gite);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_grotte":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_grotte);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_lac":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_lac);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_lieu":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_lieu);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_meteo":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_meteo);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_produit":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_produit);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_refuge":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_refuge);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_sae":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_sae);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_source":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_source);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_virtuel":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_virtuel);
+            olItem.setMarker(newMarker);
+            break;
+
+          case "icon_webcam":
+            newMarker = this.getResources().getDrawable(R.drawable.icon_webcam);
             olItem.setMarker(newMarker);
             break;
 
@@ -216,6 +341,7 @@ public class MapActivity extends Activity implements View.OnClickListener {
 
   public void onResume(){
     super.onResume();
+
   }
 
 
