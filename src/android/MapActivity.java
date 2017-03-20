d91e8e9a28f8ff86e2e1c54683ad863d05f105b2
Nicolas <nicolas13008@gmail.com>
2017-03-20 11:33:05 +0100

    add layout and code in runnable

--- src/android/MapActivity.java
+++ src/android/MapActivity.java
@@ -7,7 +7,7 @@ import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
--- src/android/nativemap.java
+++ src/android/nativemap.java
@@ -1,25 +1,15 @@
 package com.webfit.nativemap;
-import org.apache.cordova.CordovaPlugin;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
-
-
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-
-import android.preference.PreferenceManager;
-import android.util.Log;
-
-import com.google.android.gms.phenotype.Configuration;
-import com.ionicframework.camptocamp893008.R;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.views.MapView;
 
 import java.io.File;
 
@@ -28,11 +18,11 @@ public class nativemap extends CordovaPlugin {
   CallbackContext cbContext;
 
 
-  public boolean execute(String action, final JSONArray args, CallbackContext callbackContext) {
+  public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) {
     final CordovaPlugin that = this;
     this.cbContext = callbackContext;
 
-    PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);;
+    PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
     result.setKeepCallback(true);
 
     if(action.equals("startMap"))
@@ -68,30 +58,102 @@ public class nativemap extends CordovaPlugin {
       callbackContext.sendPluginResult(result);
 
     }
+
     if(action.equals("getCacheSize")) {
-      JSONObject r = new JSONObject();
-      try {
-        r.put("size", this.initializeCache((that.cordova.getActivity().getBaseContext())));
-        callbackContext.success(r);
-      } catch (JSONException e) {
-        e.printStackTrace();
-        result = new PluginResult(Status.INVALID_ACTION);
-        callbackContext.sendPluginResult(result);
-      }
+      cordova.getThreadPool().execute(new Runnable() {
+        @Override
+        public void run() {
+          JSONObject r = new JSONObject();
+          try {
+
+            r.put("size", this.initializeCache((that.cordova.getActivity().getBaseContext())));
+            callbackContext.success(r);
+          } catch (JSONException e) {
+            e.printStackTrace();
+            PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+            result.setKeepCallback(true);
+            result = new PluginResult(Status.INVALID_ACTION);
+            callbackContext.sendPluginResult(result);
+          }
+
+        }
+
+        private long initializeCache(Context context) {
+          long size = 0;
+          size += getDirSize(context.getCacheDir());
+          size += getDirSize(context.getExternalCacheDir());
+          return size;
+        }
+
+        public long getDirSize(File dir) {
+          long size = 0;
+          for (File file : dir.listFiles()) {
+            if (file != null && file.isDirectory()) {
+              size += getDirSize(file);
+            } else if (file != null && file.isFile()) {
+              size += file.length();
+            }
+          }
+          return size;
+        }
 
 
+      });
     }
     if(action.equals("clearCache")) {
-      if(this.deleteCache(that.cordova.getActivity().getBaseContext()))
-      {
-        result = new PluginResult(Status.INVALID_ACTION);
-        callbackContext.sendPluginResult(result);
-      }
-      else
-      {
-        result = new PluginResult(Status.INVALID_ACTION);
-        callbackContext.sendPluginResult(result);
-      }
+      cordova.getThreadPool().execute(new Runnable() {
+
+        @Override
+        public void run() {
+          PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+          result.setKeepCallback(true);
+          if(this.deleteCache(that.cordova.getActivity().getBaseContext()))
+          {
+            result = new PluginResult(Status.OK);
+            callbackContext.sendPluginResult(result);
+          }
+          else
+          {
+            result = new PluginResult(Status.INVALID_ACTION);
+            callbackContext.sendPluginResult(result);
+          }
+        }
+
+
+        public  boolean deleteCache(Context context) {
+          try {
+            File dir = context.getCacheDir();
+            deleteDir(dir);
+          } catch (Exception e) { return false;}
+
+          try {
+            File dir = context.getExternalCacheDir();
+            deleteDir(dir);
+          }
+          catch(Exception e ){}
+          return true;
+        }
+
+        public boolean deleteDir(File dir) {
+          if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+              boolean success = deleteDir(new File(dir, children[i]));
+              if (!success) {
+                return false;
+              }
+            }
+            return dir.delete();
+          } else if(dir!= null && dir.isFile()) {
+            return dir.delete();
+          } else {
+            return false;
+          }
+        }
+
+
+      });
+
     }
     else {
       result = new PluginResult(Status.INVALID_ACTION);
@@ -106,56 +168,6 @@ public class nativemap extends CordovaPlugin {
 
 
 
-
-  public static boolean deleteCache(Context context) {
-    try {
-      File dir = context.getCacheDir();
-      deleteDir(dir);
-    } catch (Exception e) { return false;}
-
-    try {
-      File dir = context.getExternalCacheDir();
-      deleteDir(dir);
-    }
-    catch(Exception e ){}
-    return true;
-  }
-
-  public static boolean deleteDir(File dir) {
-    if (dir != null && dir.isDirectory()) {
-      String[] children = dir.list();
-      for (int i = 0; i < children.length; i++) {
-        boolean success = deleteDir(new File(dir, children[i]));
-        if (!success) {
-          return false;
-        }
-      }
-      return dir.delete();
-    } else if(dir!= null && dir.isFile()) {
-      return dir.delete();
-    } else {
-      return false;
-    }
-  }
-
-  private long initializeCache(Context context) {
-    long size = 0;
-    size += getDirSize(context.getCacheDir());
-    size += getDirSize(context.getExternalCacheDir());
-    return size;
-  }
-
-  public long getDirSize(File dir){
-    long size = 0;
-    for (File file : dir.listFiles()) {
-      if (file != null && file.isDirectory()) {
-        size += getDirSize(file);
-      } else if (file != null && file.isFile()) {
-        size += file.length();
-      }
-    }
-    return size;
-  }
 
 
 }
